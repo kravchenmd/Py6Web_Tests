9c886c80c03b671008759d9e97bff3f75b3af96d
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2022-12-06 17:19:21 +0100

    Add async test and finish all tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,6 +4,24 @@ from src.example.ops import add, sub, mul, div, async_add
 
 class TestExample(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Set up class"""
+        print("Set up class (before all tests)")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Tear down class"""
+        print("Tear down class (after all tests)")
+
+    def setUp(self) -> None:
+        """Set up for each test"""
+        # print("Set up")
+
+    def tearDown(self) -> None:
+        """Tear down for each test"""
+        # print("Tear down")
+
     def test_add(self):
         """Add function test"""
         self.assertEqual(add(2, 3), 5)
@@ -19,3 +37,10 @@ class TestExample(unittest.TestCase):
     def test_div(self):
         """Sub function test"""
         self.assertAlmostEqual(div(2, 3), 0.66666666)
+
+
+class TestAsyncExample(unittest.IsolatedAsyncioTestCase):
+
+    async def test_async_add(self):
+        result = await async_add(2, 3)
+        self.assertEqual(5, result)
